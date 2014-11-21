4babb5170542f458efe294812cc468db8375944f
Sean <seanniesen@gmail.com>
2014-11-20 18:00:34 -0800

    add marker to map

--- js/scripts.js
+++ js/scripts.js
@@ -159,3 +159,28 @@ randomtext();
 
 
 });
+      function initialize() {
+        var mapCanvas = document.getElementById('map-canvas');
+        var myLatlng = new google.maps.LatLng(45.422615, -122.716294);
+        var mapOptions = {
+          center: new google.maps.LatLng(45.422615, -122.716294),
+          zoom: 14,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions)
+
+        var marker = new google.maps.Marker({
+    			position: myLatlng,
+    			map: map,
+    			title:"Hello World!"
+				});
+
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+
+
+
