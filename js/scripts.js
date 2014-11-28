9b3938c45576f94cf6b6069bd2a5dd58a81c6743
Sean <seanniesen@gmail.com>
2014-11-27 22:56:16 -0800

    git icons aligned

--- js/scripts.js
+++ js/scripts.js
@@ -114,6 +114,26 @@ function randomtext() {
 
 randomtext();
 
+$(".fa-phone").click(function() {
+	$(".fa-phone").addClass("hidden");
+	$(".phone").removeClass("hidden");
+});
+
+$(".phone").click(function() {
+	$(".phone").addClass("hidden");
+	$(".fa-phone").removeClass("hidden");
+});
+
+$(".fa-envelope").click(function() {
+	$(".fa-envelope").addClass("hidden");
+	$(".email").removeClass("hidden");
+});
+
+$(".email").click(function() {
+	$(".email").addClass("hidden");
+	$(".fa-envelope").removeClass("hidden");
+});
+
 
 	$('#turn-off').click(function() {
 		$('.pinkcloud1').removeClass('pinkcloud1');
