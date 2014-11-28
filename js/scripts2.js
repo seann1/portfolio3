05b45c1942005231dfb239f5ffc72b02c3eb3ee2
Sean <seanniesen@gmail.com>
2014-11-28 01:30:54 -0800

    add shuffle to about page

--- js/scripts2.js
+++ js/scripts2.js
@@ -20,4 +20,26 @@ $(document).ready(function() {
 		$(".fa-envelope").removeClass("hidden");
 	});
 
+	$("#shuffle, #shuffle3, #shuffle4, #shuffle5, #shuffle6, #shuffle7, #shuffle8, #shuffle9, #shuffle10, #shuffle11, #shuffle12, #shuffle13, #shuffle14, #shuffle15, #shuffle16, #shuffle17, #shuffle18, #shuffle19").shuffleLetters();
+
+
 });
+
+     function initialize() {
+        var mapCanvas = document.getElementById('map-canvas');
+        var myLatlng = new google.maps.LatLng(45.422615, -122.716294);
+        var mapOptions = {
+          center: new google.maps.LatLng(45.422615, -122.716294),
+          zoom: 10,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions)
+
+        var marker = new google.maps.Marker({
+    			position: myLatlng,
+    			map: map,
+    			title:"Sean"
+				});
+
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
