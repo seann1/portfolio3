6f160bf8c05f4adf5a1e443b59b6d3d453a18bed
Sean <seanniesen@gmail.com>
2014-12-05 22:19:56 -0800

    add top of page button

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,6 +1,30 @@
 
 
 $(document).ready(function() {
+
+// var windw = this;
+
+// $.fn.followTo = function ( pos ) {
+//     var $this = this,
+//         $window = $(windw);
+    
+//     $window.scroll(function(e){
+//         if ($window.scrollTop() > pos) {
+//             $this.css({
+//                 position: 'absolute',
+//                 top: pos
+//             });
+//         } else {
+//             $this.css({
+//                 position: 'fixed',
+//                 // top: 0
+//             });
+//         }
+//     });
+// };
+
+
+
 	$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
@@ -29,17 +53,33 @@ $(document).ready(function() {
 
 	$("#shuffle, #shuffle3, #shuffle4, #shuffle5, #shuffle6, #shuffle7, #shuffle8, #shuffle9, #shuffle10, #shuffle11, #shuffle12, #shuffle13, #shuffle14, #shuffle15, #shuffle16, #shuffle17, #shuffle18, #shuffle19").shuffleLetters();
 
-	function httpGet(theUrl)
-	{
-	    var xmlHttp = null;
+	// function httpGet(theUrl)
+	// {
+	//     var xmlHttp = null;
 
-	    xmlHttp = new XMLHttpRequest();
-	    xmlHttp.open( "GET", theUrl, false );
-	    xmlHttp.send( null );
-	    return xmlHttp.responseText;
-	}
-	var getresponse = httpGet("http://google.com");
-	$(".api").text(getresponse);
+	//     xmlHttp = new XMLHttpRequest();
+	//     xmlHttp.open( "GET", theUrl, false );
+	//     xmlHttp.send( null );
+	//     return xmlHttp.responseText;
+	// }
+	// var getresponse = httpGet("http://google.com");
+	// $(".api").text(getresponse);
+
+	$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+	});
+	$('#return-to-top').click(function() {      // When arrow is clicked
+	    $('body,html').animate({
+	        scrollTop : 0                       // Scroll to top of body
+	    }, 500);
+	});
+
+	$('#return-to-top').followTo(700);
+
 
 });
 
