97364962556b95336c8b95b09d95499fada09da9
Sean <seanniesen@gmail.com>
2015-01-03 11:37:22 -0800

    add contact div and refactor scollto javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,108 +1,105 @@
 $(document).ready(function() {
 
-	$(".home-link").click(function() {
-    $('html, body').animate({
-   		scrollTop: $("#index").offset().top
-    }, 700);
-	});
+  $(".home-link").click(function() {
+    scrollToDiv('home');
+  });
 
-	$(".portfolio-link").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#portfolio").offset().top
-    }, 700);
-	});
+  $(".portfolio-link").click(function() {
+    scrollToDiv('portfolio');
+  });
 
-	$(".about-link").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#about").offset().top
-    }, 700);
-	});
+  $(".about-link").click(function() {
+    scrollToDiv('about');
+  });
 
-	$(".contact-link").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#contact").offset().top
-    }, 700);
-	});
+  $(".contact-link").click(function() {
+    scrollToDiv('contact');
+  });
 
 // add active class to li in navbar on click
-	$(".nav a").on("click", function(){
+  $(".nav a").on("click", function(){
    $(".nav").find(".active").removeClass("active");
    $(this).parent().addClass("active");
 });
 
+function scrollToDiv(divid) {
+  $('html, body').animate({
+        scrollTop: $("#" + divid).offset().top - 50
+    }, 700);
+}
 
-	$("#shuffle").shuffleLetters();
+  $("#shuffle").shuffleLetters();
 
 
 //show phone and email on click of icons in footer
 $(".fa-phone").click(function() {
-	$(".fa-phone").addClass("hidden");
-	$(".phone").removeClass("hidden");
+  $(".fa-phone").addClass("hidden");
+  $(".phone").removeClass("hidden");
 });
 
 $(".phone").click(function() {
-	$(".phone").addClass("hidden");
-	$(".fa-phone").removeClass("hidden");
+  $(".phone").addClass("hidden");
+  $(".fa-phone").removeClass("hidden");
 });
 
 $(".fa-envelope").click(function() {
-	$(".fa-envelope").addClass("hidden");
-	$(".email").removeClass("hidden");
+  $(".fa-envelope").addClass("hidden");
+  $(".email").removeClass("hidden");
 });
 
 $(".email").click(function() {
-	$(".email").addClass("hidden");
-	$(".fa-envelope").removeClass("hidden");
+  $(".email").addClass("hidden");
+  $(".fa-envelope").removeClass("hidden");
 });
 
 
 
-	var randomText = $(".randomized")
+  var randomText = $(".randomized")
 
-	randomText.shuffleLetters();
-	$(".randomized").glitch();
+  randomText.shuffleLetters();
+  $(".randomized").glitch();
 
-	
-	var timesRun = 0;
-	
-	var interval = setInterval(function() {
-		timesRun += 1;
-		if(timesRun === 5){
+  
+  var timesRun = 0;
+  
+  var interval = setInterval(function() {
+    timesRun += 1;
+    if(timesRun === 5){
         clearInterval(interval);
     }
     $(".bluecloud1").glitch2();
-	}, 8000);
+  }, 8000);
 
-	setInterval(function() {
-		var d = new Date();
-		var n = d.getTime();
-		n = n.toString();
-		if ((parseInt(n.charAt(10)) % 2 === 0) && (parseInt(n.charAt(9)) % 4 > 2) && (parseInt(n.charAt(11)) / 2 == 2))  {
-			if ($('body').hasClass("demo")) {
-			randomBackground = ["glitch-background", "glitch-background2", "glitch-background3", "glitch-background4", "glitch-background5"]
-		} else {randomBackground = []};
-				$(".body").addClass(randomBackground[Math.floor(Math.random() * randomBackground.length)]); 
-		}
-		else { $(".body").removeClass("glitch-background");
-					 $(".body").removeClass("glitch-background2");
-					 $(".body").removeClass("glitch-background3");
-					 $(".body").removeClass("glitch-background4");
-					 $(".body").removeClass("glitch-background5");
+  setInterval(function() {
+    var d = new Date();
+    var n = d.getTime();
+    n = n.toString();
+    if ((parseInt(n.charAt(10)) % 2 === 0) && (parseInt(n.charAt(9)) % 4 > 2) && (parseInt(n.charAt(11)) / 2 == 2))  {
+      if ($('body').hasClass("demo")) {
+      randomBackground = ["glitch-background", "glitch-background2", "glitch-background3", "glitch-background4", "glitch-background5"]
+    } else {randomBackground = []};
+        $(".body").addClass(randomBackground[Math.floor(Math.random() * randomBackground.length)]); 
+    }
+    else { $(".body").removeClass("glitch-background");
+           $(".body").removeClass("glitch-background2");
+           $(".body").removeClass("glitch-background3");
+           $(".body").removeClass("glitch-background4");
+           $(".body").removeClass("glitch-background5");
 
-		}
+    }
 
-	}, 20);
+  }, 20);
 
-	setInterval(function() {
-		var randomWord = ["<h1>Coder</h1>", "<h1>Designer</h1>", "<h1>Engineer</h1>", "<h1>Problem Solver</h1>", "<h1>Artist</h1>"]
-		$("#subtitle").html(randomWord[Math.floor(Math.random() * randomWord.length)]).shuffleLetters();
+  setInterval(function() {
+    var randomWord = ["<h1>Coder</h1>", "<h1>Designer</h1>", "<h1>Engineer</h1>", "<h1>Problem Solver</h1>", "<h1>Artist</h1>"]
+    $("#subtitle").html(randomWord[Math.floor(Math.random() * randomWord.length)]).shuffleLetters();
 
-	}, 5000);
+  }, 5000);
 
 
-	//audio
+  //audio
 
-	var audioElement = document.createElement('audio');
+  var audioElement = document.createElement('audio');
   audioElement.setAttribute('src', 'sounds/sound3.wav');
 
   var audioElement1 = document.createElement('audio');
@@ -131,8 +128,8 @@ $(".email").click(function() {
 
 
         $('.play').click(function() {
-        	var chosenSound = randomSound[Math.floor(Math.random() * randomSound.length)];
-        	chosenSound.volume = 0.5;
+          var chosenSound = randomSound[Math.floor(Math.random() * randomSound.length)];
+          chosenSound.volume = 0.5;
             chosenSound.play();
         });
 
@@ -151,10 +148,10 @@ $(".email").click(function() {
         var map = new google.maps.Map(mapCanvas, mapOptions)
 
         var marker = new google.maps.Marker({
-    			position: myLatlng,
-    			map: map,
-    			title:"Sean"
-				});
+          position: myLatlng,
+          map: map,
+          title:"Sean"
+        });
 
       }
       google.maps.event.addDomListener(window, 'load', initialize);
