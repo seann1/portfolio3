a90a821858db96b280829a32e47539ed598d3eae
Sean <seanniesen@gmail.com>
2015-01-08 13:47:26 -0800

    move position of return-to-top div

--- js/scripts.js
+++ js/scripts.js
@@ -59,37 +59,6 @@ $(".email").click(function() {
   randomText.shuffleLetters();
   $(".randomized").glitch();
 
-  
-  var timesRun = 0;
-  
-  var interval = setInterval(function() {
-    timesRun += 1;
-    if(timesRun === 5){
-        clearInterval(interval);
-    }
-    $(".bluecloud1").glitch2();
-  }, 8000);
-
-  setInterval(function() {
-    var d = new Date();
-    var n = d.getTime();
-    n = n.toString();
-    if ((parseInt(n.charAt(10)) % 2 === 0) && (parseInt(n.charAt(9)) % 4 > 2) && (parseInt(n.charAt(11)) / 2 == 2))  {
-      if ($('body').hasClass("demo")) {
-      randomBackground = ["glitch-background", "glitch-background2", "glitch-background3", "glitch-background4", "glitch-background5"]
-    } else {randomBackground = []};
-        $(".body").addClass(randomBackground[Math.floor(Math.random() * randomBackground.length)]); 
-    }
-    else { $(".body").removeClass("glitch-background");
-           $(".body").removeClass("glitch-background2");
-           $(".body").removeClass("glitch-background3");
-           $(".body").removeClass("glitch-background4");
-           $(".body").removeClass("glitch-background5");
-
-    }
-
-  }, 20);
-
   setInterval(function() {
     var randomWord = ["<h1>Coder</h1>", "<h1>Designer</h1>", "<h1>Engineer</h1>", "<h1>Problem Solver</h1>", "<h1>Artist</h1>"]
     $("#subtitle").html(randomWord[Math.floor(Math.random() * randomWord.length)]).shuffleLetters();
@@ -98,40 +67,6 @@ $(".email").click(function() {
 
 
   //audio
-
-  var audioElement = document.createElement('audio');
-  audioElement.setAttribute('src', 'sounds/sound3.wav');
-
-  var audioElement1 = document.createElement('audio');
-  audioElement1.setAttribute('src', 'sounds/sound1.wav');
-
-  var audioElement2 = document.createElement('audio');
-  audioElement2.setAttribute('src', 'sounds/sound4.wav');
-
-  var audioElement3 = document.createElement('audio');
-  audioElement3.setAttribute('src', 'sounds/sound5.wav');
-
-  var audioElement4 = document.createElement('audio');
-  audioElement4.setAttribute('src', 'sounds/sound6.wav');
-
-  var audioElement5 = document.createElement('audio');
-  audioElement5.setAttribute('src', 'sounds/sound7.wav');
-
-  var audioElement6 = document.createElement('audio');
-  audioElement6.setAttribute('src', 'sounds/sound8.wav');
-
-  var audioElement7 = document.createElement('audio');
-  audioElement7.setAttribute('src', 'sounds/sound9.wav');
-
-  var randomSound = [audioElement, audioElement1, audioElement2, audioElement3, audioElement4, audioElement5, audioElement6, audioElement7];
-
-
-
-        $('.play').click(function() {
-          var chosenSound = randomSound[Math.floor(Math.random() * randomSound.length)];
-          chosenSound.volume = 0.5;
-            chosenSound.play();
-        });
 
 });
 
--- js/scripts2.js
+++ js/scripts2.js
@@ -9,27 +9,6 @@ $(document).ready(function() {
     $(this).ekkoLightbox();
 	}); 
 
-
-	// $(".fa-phone").click(function() {
-	// 	$(".fa-phone").addClass("hidden");
-	// 	$(".phone").removeClass("hidden");
-	// });
-
-	// $(".phone").click(function() {
-	// 	$(".phone").addClass("hidden");
-	// 	$(".fa-phone").removeClass("hidden");
-	// });
-
-	// $(".fa-envelope").click(function() {
-	// 	$(".fa-envelope").addClass("hidden");
-	// 	$(".email").removeClass("hidden");
-	// });
-
-	// $(".email").click(function() {
-	// 	$(".email").addClass("hidden");
-	// 	$(".fa-envelope").removeClass("hidden");
-	// });
-
 	$("#shuffle, #shuffle3, #shuffle4, #shuffle5, #shuffle6, #shuffle7, #shuffle8, #shuffle9, #shuffle10, #shuffle11, #shuffle12, #shuffle13, #shuffle14, #shuffle15, #shuffle16, #shuffle17, #shuffle18, #shuffle19").shuffleLetters();
 
 	$(window).scroll(function() {
@@ -37,6 +16,12 @@ $(document).ready(function() {
         $('#return-to-top').fadeIn(200);    // Fade in the arrow
     } else {
         $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+
+    if ($(this).scrollTop() >= 2000) {
+        $('#return-to-top').css({"bottom": "12px", "right": "20px"});
+    } else {
+       $('#return-to-top').css({"bottom": "-135px", "right": "20px"}); 
     }
 	});
 	$('#return-to-top').click(function() {      // When arrow is clicked
