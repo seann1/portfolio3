02a9663d3ed3fb6515f03db2cfbcada740bdbb43
Sean <seanniesen@gmail.com>
2015-01-08 18:19:36 -0800

    make move-to-top rotate

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+  var scrollpos = $("#home").scrollTop();
+
   $(".home-link").click(function() {
     scrollToDiv('home');
   });
@@ -66,7 +68,32 @@ $(".email").click(function() {
   }, 5000);
 
 
-  //audio
+  $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+  }); 
+
+  $("#shuffle, #shuffle3, #shuffle4, #shuffle5, #shuffle6, #shuffle7, #shuffle8, #shuffle9, #shuffle10, #shuffle11, #shuffle12, #shuffle13, #shuffle14, #shuffle15, #shuffle16, #shuffle17, #shuffle18, #shuffle19").shuffleLetters();
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) { 
+        $('#return-to-top').fadeIn(1000);       // If page is scrolled more than 50px
+    } else {
+        // $('.uparrow').animate({opacity: "0%"}, 3000);   // Else fade out the arrow
+        $('#return-to-top').fadeOut(1000);
+    }
+
+    if ($(this).scrollTop() >= 2000) {
+        $('#return-to-top').css({"bottom": "12px", "right": "20px"});
+    } else {
+       $('#return-to-top').css({"bottom": "-135px", "right": "20px"}); 
+    }
+  });
+  $('#return-to-top').click(function() {      // When arrow is clicked
+      $('body,html').animate({
+          scrollTop : 0                       // Scroll to top of body
+      }, 500);
+  });
 
 });
 
--- js/scripts2.js
+++ js/scripts2.js
@@ -3,32 +3,4 @@
 $(document).ready(function() {
 
 
-
-	$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
-	}); 
-
-	$("#shuffle, #shuffle3, #shuffle4, #shuffle5, #shuffle6, #shuffle7, #shuffle8, #shuffle9, #shuffle10, #shuffle11, #shuffle12, #shuffle13, #shuffle14, #shuffle15, #shuffle16, #shuffle17, #shuffle18, #shuffle19").shuffleLetters();
-
-	$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
-    } else {
-        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-    }
-
-    if ($(this).scrollTop() >= 2000) {
-        $('#return-to-top').css({"bottom": "12px", "right": "20px"});
-    } else {
-       $('#return-to-top').css({"bottom": "-135px", "right": "20px"}); 
-    }
-	});
-	$('#return-to-top').click(function() {      // When arrow is clicked
-	    $('body,html').animate({
-	        scrollTop : 0                       // Scroll to top of body
-	    }, 500);
-	});
-
-
 });
