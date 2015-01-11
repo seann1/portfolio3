80f881c3c29d3798e5ea8482ee83a87e026fef71
Sean <seanniesen@gmail.com>
2015-01-10 23:38:42 -0800

    make links active on scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
 
-  var scrollpos = $("#home").scrollTop();
+
+$(document).ready(function() {
 
   $(".home-link").click(function() {
     scrollToDiv('home');
@@ -56,13 +56,13 @@ $(".email").click(function() {
 
 
 
-  var randomText = $(".randomized")
+  var randomText = $(".randomized");
 
   randomText.shuffleLetters();
   $(".randomized").glitch();
 
   setInterval(function() {
-    var randomWord = ["<h1>Coder</h1>", "<h1>Designer</h1>", "<h1>Engineer</h1>", "<h1>Problem Solver</h1>", "<h1>Artist</h1>"]
+    var randomWord = ["<h1>Coder</h1>", "<h1>Designer</h1>", "<h1>Engineer</h1>", "<h1>Problem Solver</h1>", "<h1>Artist</h1>"];
     $("#subtitle").html(randomWord[Math.floor(Math.random() * randomWord.length)]).shuffleLetters();
 
   }, 5000);
@@ -71,12 +71,12 @@ $(".email").click(function() {
   $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
-  }); 
+  });
 
   $("#shuffle, #shuffle3, #shuffle4, #shuffle5, #shuffle6, #shuffle7, #shuffle8, #shuffle9, #shuffle10, #shuffle11, #shuffle12, #shuffle13, #shuffle14, #shuffle15, #shuffle16, #shuffle17, #shuffle18, #shuffle19").shuffleLetters();
 
   $(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) { 
+    if ($(this).scrollTop() >= 50) {
         $('#return-to-top').fadeIn(1000);       // If page is scrolled more than 50px
     } else {
         // $('.uparrow').animate({opacity: "0%"}, 3000);   // Else fade out the arrow
@@ -86,14 +86,44 @@ $(".email").click(function() {
     if ($(this).scrollTop() >= 2000) {
         $('#return-to-top').css({"bottom": "12px", "right": "20px"});
     } else {
-       $('#return-to-top').css({"bottom": "-135px", "right": "20px"}); 
+       $('#return-to-top').css({"bottom": "-135px", "right": "20px"});
     }
+
+  });
+
+  $(window).on('scroll', function() {
+    var scrollTop = $(window).scrollTop(),
+        portfolioOffset = $('#portfolio').offset().top,
+        portfolioDistance  = (portfolioOffset - scrollTop),
+        aboutOffset = $("#about").offset().top,
+        aboutDistance = (aboutOffset - scrollTop),
+        contactOffset = $('#contact').offset().top,
+        contactDistance = (contactOffset - scrollTop),
+        homeOffset = $('#home').offset().top,
+        homeDistance = (homeOffset - scrollTop);
+
+        if (homeDistance > -50 && homeDistance < 50) {
+          $(".nav").find(".active").removeClass("active");
+          $(".home-link").addClass("active");
+        } else if (portfolioDistance > -50 && portfolioDistance < 50) {
+          $(".nav").find(".active").removeClass("active");
+          $(".portfolio-link").addClass("active");
+        } else if (aboutDistance > -50 && aboutDistance < 50) {
+          $(".nav").find(".active").removeClass("active");
+          $(".about-link").addClass("active");
+        } else if (contactDistance > -50 && contactDistance < 50) {
+          $(".nav").find(".active").removeClass("active");
+          $(".contact-link").addClass("active");
+        } else {
+
+        }
   });
   $('#return-to-top').click(function() {      // When arrow is clicked
       $('body,html').animate({
           scrollTop : 0                       // Scroll to top of body
       }, 500);
   });
+
 
 });
 
@@ -106,8 +136,8 @@ $(".email").click(function() {
           center: new google.maps.LatLng(45.422615, -122.716294),
           zoom: 10,
           mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-        var map = new google.maps.Map(mapCanvas, mapOptions)
+        };
+        var map = new google.maps.Map(mapCanvas, mapOptions);
 
         var marker = new google.maps.Marker({
           position: myLatlng,
