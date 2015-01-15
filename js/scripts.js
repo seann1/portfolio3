001a1a1c1ab2dce181cc57dd850d8081b1969a45
Sean <seanniesen@gmail.com>
2015-01-14 21:04:07 -0800

    fixing problem with modal and z index

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,19 @@
 
 $(document).ready(function() {
 
+  jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
+                                                $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
+                                                $(window).scrollLeft()) + "px");
+    return this;
+  }
   $(".large-home-screen").hide();
 
   $(".iphone-home-thumb").click(function() {
     $(".large-home-screen").show();
+    $(".large-home-screen").center();
   });
 
   $(".large-home-screen").click(function() {
